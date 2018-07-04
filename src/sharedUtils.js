ea64e5b1625c0c5eca3e669e1f3db96091dbbc92
Eric Biewener <eric@talentpair.com>
2018-07-04 15:15:43 -0700

    Use regex

--- src/sharedUtils.js
+++ src/sharedUtils.js
@@ -2,21 +2,27 @@ const path = require('path')
 const {window, Range, Position} = require('vscode')
 const _ = require('lodash')
 
-async function insertLine(newLine, linePosition, lines) {
-  const {start, lineIndexModifier, isFirstImportLine} = linePosition
-  const end = linePosition.end || start
-  
-  await window.activeTextEditor.edit(builder => {
-    if (!lineIndexModifier) {
-      builder.replace(new Range(start, 0, end, lines[end].length), newLine)
-    } else if (lineIndexModifier === 1) {
-      builder.insert(new Position(end, lines[end].length), '\n' + newLine)
-    } else { // -1
-      // If it's the first import line, then add an extra new line between it and the subsequent non-import code.
-      // We only need to worry about this here, because if `isFirstImportLine` = true, the only alternative
-      // `lineIndexModifier` is 1, which occurs when the file only has comments
-      const extraNewLine = isFirstImportLine ? '\n' : ''
-      builder.insert(new Position(end, 0), newLine + '\n' + extraNewLine)
+async function insertLine(newLine, importPosition) {
+  const {match, indexModifier} = importPosition
+  const editor = window.activeTextEditor
+  const {document} = editor
+
+  await editor.edit(builder => {
+    if (!match) {
+      builder.insert(new Position(0, 0), newLine + '\n')
+    } else if (!indexModifier) {
+      builder.replace(
+        new Range(
+          document.positionAt(match.start),
+          document.positionAt(match.end - 1)
+        ),
+        newLine
+      )
+    } else if (indexModifier === 1) {
+      builder.insert(document.positionAt(match.end - 1), '\n' + newLine)
+    } else {
+      // -1
+      builder.insert(document.positionAt(match.start), newLine + '\n')
     }
   })
 }
@@ -27,25 +33,26 @@ function getTabChar() {
 }
 
 function strBetween(str, startChar, endChar) {
-  const start = typeof startChar === 'string'
-    ? str.indexOf(startChar)
-    : str.search(startChar)
+  const start =
+    typeof startChar === 'string'
+      ? str.indexOf(startChar)
+      : str.search(startChar)
   if (start < 0) return
-  
+
   const substr = str.slice(start + 1)
-  
+
   endChar = endChar || startChar
-  const end = typeof endChar === 'string'
-    ? substr.indexOf(endChar || startChar)
-    : substr.search(endChar || startChar)
-  
+  const end =
+    typeof endChar === 'string'
+      ? substr.indexOf(endChar || startChar)
+      : substr.search(endChar || startChar)
+
   return end < 0 ? substr : substr.slice(0, end)
 }
 
 function strUntil(str, endChar) {
-  const index = typeof endChar === 'string'
-    ? str.indexOf(endChar)
-    : str.search(endChar)
+  const index =
+    typeof endChar === 'string' ? str.indexOf(endChar) : str.search(endChar)
   return index < 0 ? str : str.slice(0, index)
 }
 
@@ -59,5 +66,5 @@ module.exports = {
   getTabChar,
   strBetween,
   strUntil,
-  removeExt,
+  removeExt
 }
