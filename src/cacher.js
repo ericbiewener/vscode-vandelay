3addd2bbf2a136f91fad573c3760e7cacf74089b
Eric Biewener <eric@talentpair.com>
2018-07-19 23:41:46 -0700

    Add es5 require test workspace

--- src/cacher.js
+++ src/cacher.js
@@ -1,61 +1,82 @@
-const {window, workspace} = require('vscode')
+const { window, workspace } = require('vscode')
 const path = require('path')
 const fs = require('fs-extra')
 const _ = require('lodash')
 const anymatch = require('anymatch')
-const {writeCacheFile, getLangFromFilePath, getFilepathKey} = require('./utils')
-const {cacheFileManager} = require('./cacheFileManager')
-const {PLUGINS} = require('./plugins')
+const {
+  writeCacheFile,
+  getLangFromFilePath,
+  getFilepathKey,
+} = require('./utils')
+const { cacheFileManager } = require('./cacheFileManager')
+const { PLUGINS } = require('./plugins')
 
 function shouldIgnore(plugin, filePath) {
   return anymatch(plugin.excludePatterns, filePath)
 }
 
-function cacheDir(plugin, dir, recursive=true, data={_extraImports: {}}) {
-  return fs.readdir(dir).then(items => {
-    const readDirPromises = []
+function cacheDir(plugin, dir, recursive = true, data = { _extraImports: {} }) {
+  return fs
+    .readdir(dir)
+    .then(items => {
+      const readDirPromises = []
 
-    for (const item of items) {
-      const fullPath = path.join(dir, item)
-      if (shouldIgnore(plugin, fullPath)) continue
+      for (const item of items) {
+        const fullPath = path.join(dir, item)
+        if (shouldIgnore(plugin, fullPath)) continue
 
-      readDirPromises.push(
-        fs.lstat(fullPath).then(stats => {
-          if (stats.isFile()) {
-            if (plugin.language !== getLangFromFilePath(item) || item === plugin.configFile) return
-            plugin.cacheFile(plugin, fullPath, data)
-          }
-          else if (recursive) {
-            return cacheDir(plugin, fullPath, true, data)
-          }
-        })
-      )
-    }
+        readDirPromises.push(
+          fs.lstat(fullPath).then(stats => {
+            if (stats.isFile()) {
+              if (
+                plugin.language !== getLangFromFilePath(item) ||
+                item === plugin.configFile
+              )
+                return
+              plugin.cacheFile(plugin, fullPath, data)
+            } else if (recursive) {
+              return cacheDir(plugin, fullPath, true, data)
+            }
+          })
+        )
+      }
 
-    return Promise.all(readDirPromises)
-  })
+      return Promise.all(readDirPromises)
+    })
     .then(() => data)
 }
 
 function cacheProjectLanguage(plugin) {
-  let cacher = Promise.all(plugin.includePaths.map(p => cacheDir(plugin, p)))
-    .then(exportObjArrays => (
-      exportObjArrays.reduce((acc, obj) => Object.assign(acc, obj), {})
-    ))
+  let cacher = Promise.all(
+    plugin.includePaths.map(p => cacheDir(plugin, p))
+  ).then(exportObjArrays =>
+    exportObjArrays.reduce((acc, obj) => Object.assign(acc, obj), {})
+  )
 
   if (plugin.processCachedData) cacher = cacher.then(plugin.processCachedData)
   return cacher.then(data => writeCacheFile(plugin, data))
 }
 
 function cacheProject() {
-  return Promise.all(_.map(PLUGINS, cacheProjectLanguage))
-    .then(() => window.showInformationMessage('Project exports have been cached. 🏖️'))
+  return Promise.all(_.map(PLUGINS, cacheProjectLanguage)).then(() =>
+    window.showInformationMessage('Project exports have been cached. 🏖️')
+  )
 }
 
 function onChangeOrCreate(doc) {
   const plugin = PLUGINS[getLangFromFilePath(doc.path)]
-  if (!plugin || shouldIgnore(plugin, doc.path)) return
-  
+  if (
+    !plugin ||
+    shouldIgnore(plugin, doc.path) ||
+    // TODO: Since we are watching all files in the workspace, not just those in plugin.includePaths,
+    // we need to make sure that it is actually in that array. Can this be changed so that we only
+    // watch files in plugin.includePaths to begin with? Not sure if this can be accomplished with
+    // a single glob. If not, we'd need multiple watchers. Would either case be more efficient than
+    // what we're currently doing?
+    !plugin.includePaths.some(p => doc.path.startsWith(p))
+  )
+    return
+
   const data = plugin.cacheFile(plugin, doc.path)
   if (_.isEmpty(data)) return
 
@@ -77,11 +98,11 @@ function watchForChanges() {
 
   watcher.onDidChange(onChangeOrCreate)
   watcher.onDidCreate(onChangeOrCreate)
-  
+
   watcher.onDidDelete(doc => {
     const plugin = PLUGINS[getLangFromFilePath(doc.path)]
     if (!plugin) return
-    
+
     cacheFileManager(plugin, cachedData => {
       const key = getFilepathKey(plugin, doc.path)
       if (!cachedData[key]) return
@@ -98,5 +119,5 @@ module.exports = {
   getFilepathKey,
   _test: {
     cacheDir,
-  }
+  },
 }
