5ee24cfd533d67d6210a58bbd5a62c618cbd8b05
Eric Biewener <ebiewener@gmail.com>
2019-02-12 15:41:52 -0800

    fixes

--- src/plugins/python/importing/getImportPosition.js
+++ src/plugins/python/importing/getImportPosition.js
@@ -1,7 +1,11 @@
-const _ = require("lodash");
-const { getLastInitialComment, strUntil } = require("../../../utils");
-const { commentRegex } = require("../regex");
-const { isPathPackage } = require("../utils");
+const _ = require('lodash')
+const {
+  getLastInitialComment,
+  strUntil,
+  getImportOrderPosition,
+} = require('../../../utils')
+const { commentRegex } = require('../regex')
+const { isPathPackage } = require('../utils')
 
 /**
  * Determine which line number should get the import. This could be merged into that line
@@ -15,45 +19,45 @@ function getImportPosition(plugin, importPath, isExtraImport, imports, text) {
     return {
       match: getLastInitialComment(text, commentRegex),
       indexModifier: 1,
-      isFirstImport: true
-    };
+      isFirstImport: true,
+    }
   }
 
   // First look for an exact match. This is done outside the main sorting loop because we don't care
   // where the exact match is located if it exists.
-  const exactMatch = imports.find(i => i.path === importPath);
+  const exactMatch = imports.find(i => i.path === importPath)
   if (exactMatch) {
-    return { match: exactMatch, indexModifier: 0 };
+    return { match: exactMatch, indexModifier: 0 }
   }
 
-  const importPos = getImportOrderPosition(plugin, strUntil(importPath, "."));
-  const importIsAbsolute = !importPath.startsWith(".");
+  const importPos = getImportOrderPosition(plugin, strUntil(importPath, '.'))
+  const importIsAbsolute = !importPath.startsWith('.')
 
   for (const importData of imports) {
     // Package check
-    const lineIsPackage = isPathPackage(plugin, importData.path);
-    if (lineIsPackage && !isExtraImport) continue;
+    const lineIsPackage = isPathPackage(plugin, importData.path)
+    if (lineIsPackage && !isExtraImport) continue
 
     const lineImportPos = getImportOrderPosition(
       plugin,
-      strUntil(importData.path, ".")
-    );
+      strUntil(importData.path, '.')
+    )
 
     // Both have import orders
     if (importPos != null && lineImportPos != null) {
-      if (importPos > lineImportPos) continue;
+      if (importPos > lineImportPos) continue
       return {
         match: importData,
         indexModifier:
-          importPos < lineImportPos || importPath < importData.path ? -1 : 1
-      };
+          importPos < lineImportPos || importPath < importData.path ? -1 : 1,
+      }
     }
 
     // One is a package and the other isn't
     if (isExtraImport && !lineIsPackage) {
-      return { match: importData, indexModifier: -1 };
+      return { match: importData, indexModifier: -1 }
     } else if (!isExtraImport && lineIsPackage) {
-      continue;
+      continue
     }
 
     // IF one has a position and the other doesn't...
@@ -64,36 +68,36 @@ function getImportPosition(plugin, importPath, isExtraImport, imports, text) {
         (isExtraImport && importPos != null) ||
         (!isExtraImport && lineImportPos != null)
       )
-        continue;
+        continue
       return {
         match: importData,
-        indexModifier: -1
-      };
+        indexModifier: -1,
+      }
     }
 
     if (isExtraImport && (!lineIsPackage || importPath < importData.path)) {
-      return { match: importData, indexModifier: -1 };
+      return { match: importData, indexModifier: -1 }
     } else if (lineIsPackage) {
-      continue;
+      continue
     }
 
     // Absolute path comparison. This also handles the case where both paths are packages, causing
     // them to get compared alphabetically.
-    const lineIsAbsolute = !importData.path.startsWith(".");
+    const lineIsAbsolute = !importData.path.startsWith('.')
     if (importIsAbsolute && (!lineIsAbsolute || importPath < importData.path)) {
-      return { match: importData, indexModifier: -1 };
+      return { match: importData, indexModifier: -1 }
     } else if (lineIsAbsolute) {
-      continue;
+      continue
     }
   }
 
   // Since we didn't find a line to sort the new import before, it will go after the last import
   return {
     match: _.last(imports),
-    indexModifier: 1
-  };
+    indexModifier: 1,
+  }
 }
 
 module.exports = {
-  getImportPosition
-};
+  getImportPosition,
+}
--- src/plugins/python/importing/importer.js
+++ src/plugins/python/importing/importer.js
@@ -1,72 +1,78 @@
-const { window, Range } = require("vscode");
-const path = require("path");
-const { getTabChar, strUntil, getExportDataKeysByCachedDate, removeExt } = require("../../../utils");
-const { commentRegex, parseImports } = require("../regex");
-const { getImportPosition } = require("./getImportPosition");
+const { window, Range } = require('vscode')
+const path = require('path')
+const {
+  getTabChar,
+  strUntil,
+  getExportDataKeysByCachedDate,
+  removeExt,
+  insertLine,
+} = require('../../../utils')
+const { commentRegex, parseImports } = require('../regex')
+const { getImportPosition } = require('./getImportPosition')
 
 function buildImportItems(plugin, exportData) {
-  const { projectRoot, shouldIncludeImport } = plugin;
-  const activeFilepath = window.activeTextEditor.document.fileName;
-  const items = [];
+  const { projectRoot, shouldIncludeImport } = plugin
+  const activeFilepath = window.activeTextEditor.document.fileName
+  const items = []
 
-  const sortedKeys = getExportDataKeysByCachedDate(exportData);
+  const sortedKeys = getExportDataKeysByCachedDate(exportData)
   for (const importPath of sortedKeys) {
-    const data = exportData[importPath];
+    const data = exportData[importPath]
     const absImportPath = data.isExtraImport
       ? importPath
-      : path.join(projectRoot, importPath);
-    if (absImportPath === activeFilepath) continue;
+      : path.join(projectRoot, importPath)
+    if (absImportPath === activeFilepath) continue
     if (
       shouldIncludeImport &&
       !shouldIncludeImport(absImportPath, activeFilepath)
     ) {
-      continue;
+      continue
     }
 
-    let dotPath;
+    let dotPath
     if (data.isExtraImport) {
-      dotPath = importPath;
+      dotPath = importPath
     } else {
-      dotPath = removeExt(importPath).replace(/\//g, ".");
-      if (plugin.processImportPath) dotPath = plugin.processImportPath(dotPath);
+      dotPath = removeExt(importPath).replace(/\//g, '.')
+      if (plugin.processImportPath) dotPath = plugin.processImportPath(dotPath)
     }
 
     if (data.importEntirePackage) {
       items.push({
         label: importPath,
-        isExtraImport: data.isExtraImport
-      });
+        isExtraImport: data.isExtraImport,
+      })
     }
 
-    if (!data.exports) continue;
+    if (!data.exports) continue
 
     for (const exportName of data.exports) {
       items.push({
         label: exportName,
         description: dotPath,
-        isExtraImport: data.isExtraImport
-      });
+        isExtraImport: data.isExtraImport,
+      })
     }
   }
 
-  return items;
+  return items
 }
 
 function insertImport(plugin, importSelection) {
-  const { label: exportName, isExtraImport } = importSelection;
-  const isPackageImport = !importSelection.description;
-  const importPath = importSelection.description || exportName;
-  const editor = window.activeTextEditor;
+  const { label: exportName, isExtraImport } = importSelection
+  const isPackageImport = !importSelection.description
+  const importPath = importSelection.description || exportName
+  const editor = window.activeTextEditor
 
-  const fileText = editor.document.getText();
-  const imports = parseImports(fileText);
+  const fileText = editor.document.getText()
+  const imports = parseImports(fileText)
   const importPosition = getImportPosition(
     plugin,
     importPath,
     isExtraImport,
     imports,
     fileText
-  );
+  )
 
   // Make sure we aren't importing a full package when it already has a partial import, or vice versa
   if (!importPosition.indexModifier && !importPosition.isFirstImport) {
@@ -75,54 +81,54 @@ function insertImport(plugin, importSelection) {
         // partial imports exist
         window.showErrorMessage(
           "Can't import entire package when parts of the package are already being imported."
-        );
+        )
       }
-      return;
+      return
     } else if (!importPosition.match.imports) {
       // partial imports don't exist
       window.showErrorMessage(
         "Can't import part of a package when the entire package is already being imported."
-      );
-      return;
+      )
+      return
     }
   }
 
-  const lineImports = getNewLineImports(importPosition, exportName);
-  if (!lineImports) return;
+  const lineImports = getNewLineImports(importPosition, exportName)
+  if (!lineImports) return
   let newLine = isPackageImport
     ? `import ${exportName}`
-    : getNewLine(plugin, importPath, lineImports);
+    : getNewLine(plugin, importPath, lineImports)
 
   // Import groups
 
-  const { indexModifier } = importPosition;
+  const { indexModifier } = importPosition
   // If indexModifier is 0, we're adding to a pre-existing line so no need to worry about groups
   if (indexModifier && plugin.importGroups) {
     const { before, after } = getSurroundingImportPaths(
       plugin,
       imports,
       importPosition
-    );
+    )
 
     if (before || after) {
       const beforeGroup = before
         ? findImportPathGroup(plugin, before.path)
-        : null;
-      const afterGroup = after ? findImportPathGroup(plugin, after.path) : null;
-      const newGroup = findImportPathGroup(plugin, importPath || exportName);
+        : null
+      const afterGroup = after ? findImportPathGroup(plugin, after.path) : null
+      const newGroup = findImportPathGroup(plugin, importPath || exportName)
 
-      if (before && newGroup != beforeGroup) newLine = "\n" + newLine;
-      if (after && newGroup != afterGroup) newLine += "\n";
+      if (before && newGroup != beforeGroup) newLine = '\n' + newLine
+      if (after && newGroup != afterGroup) newLine += '\n'
       // Rewrite all 3 import lines
       const beforeLine = before
         ? `${fileText.slice(before.start, before.end)}\n`
-        : "";
+        : ''
       const afterLine = after
         ? `\n${fileText.slice(after.start, after.end)}`
-        : "";
+        : ''
       return editor.edit(builder => {
         const beforeMatch =
-          before || getLastInitialComment(fileText, commentRegex);
+          before || getLastInitialComment(fileText, commentRegex)
 
         builder.replace(
           new Range(
@@ -134,89 +140,89 @@ function insertImport(plugin, importSelection) {
             editor.document.positionAt(after ? after.end : before.end)
           ),
           `${beforeLine}${newLine}${afterLine}`
-        );
-      });
+        )
+      })
     }
   }
 
-  return insertLine(newLine, importPosition);
+  return insertLine(newLine, importPosition)
 }
 
 function findImportPathGroup(plugin, importPath) {
-  const importPathPrefix = strUntil(importPath, ".");
+  const importPathPrefix = strUntil(importPath, '.')
 
   for (const group of plugin.importGroups) {
     if (group.includes(importPathPrefix)) {
-      return group;
+      return group
     }
   }
 }
 
 function getSurroundingImportPaths(plugin, imports, importPosition) {
-  const { match, indexModifier } = importPosition;
-  const matchIndex = imports.indexOf(match);
-  const before = imports[matchIndex - (indexModifier > 0 ? 0 : 1)];
-  const after = imports[matchIndex + (indexModifier > 0 ? 1 : 0)];
+  const { match, indexModifier } = importPosition
+  const matchIndex = imports.indexOf(match)
+  const before = imports[matchIndex - (indexModifier > 0 ? 0 : 1)]
+  const after = imports[matchIndex + (indexModifier > 0 ? 1 : 0)]
 
   // If a line break exists, then either before or after should be null depending on whether
   // the import is being inserted directly after `before` or directly before `after`
   return {
     before: before,
-    after: after
-  };
+    after: after,
+  }
 }
 
 function getNewLineImports(importPosition, exportName) {
-  const { match, indexModifier } = importPosition;
+  const { match, indexModifier } = importPosition
 
-  if (indexModifier) return [exportName];
-  if (match.imports.includes(exportName)) return;
-  return [...match.imports, exportName];
+  if (indexModifier) return [exportName]
+  if (match.imports.includes(exportName)) return
+  return [...match.imports, exportName]
 }
 
 function getNewLine(plugin, importPath, imports) {
-  const { maxImportLineLength } = plugin;
+  const { maxImportLineLength } = plugin
 
-  const sensitivity = { sensitivity: "base" };
-  imports.sort((a, b) => a.localeCompare(b, undefined, sensitivity));
+  const sensitivity = { sensitivity: 'base' }
+  imports.sort((a, b) => a.localeCompare(b, undefined, sensitivity))
 
-  const newLineStart = "from " + importPath + " import ";
-  const newLineEnd = imports.join(", ");
+  const newLineStart = 'from ' + importPath + ' import '
+  const newLineEnd = imports.join(', ')
 
-  const tabChar = getTabChar();
-  const newLineLength = newLineStart.length + newLineEnd.length;
+  const tabChar = getTabChar()
+  const newLineLength = newLineStart.length + newLineEnd.length
 
   if (newLineLength <= maxImportLineLength) {
-    return newLineStart + newLineEnd;
+    return newLineStart + newLineEnd
   }
 
-  let line = newLineStart + "(";
-  let fullText = "";
+  let line = newLineStart + '('
+  let fullText = ''
 
   imports.forEach((name, i) => {
-    const isLast = i === imports.length - 1;
+    const isLast = i === imports.length - 1
 
-    let newText = (i > 0 ? " " : "") + name;
-    if (!isLast) newText += ",";
+    let newText = (i > 0 ? ' ' : '') + name
+    if (!isLast) newText += ','
 
-    let newLength = line.length + newText.length;
-    if (isLast) newLength++; // for closing parenthesis
+    let newLength = line.length + newText.length
+    if (isLast) newLength++ // for closing parenthesis
 
     if (newLength < maxImportLineLength) {
-      line += newText;
+      line += newText
     } else {
-      fullText += line + "\n";
-      line = tabChar + newText.trim();
+      fullText += line + '\n'
+      line = tabChar + newText.trim()
     }
 
-    if (isLast) fullText += line;
-  });
+    if (isLast) fullText += line
+  })
 
-  return fullText + ")";
+  return fullText + ')'
 }
 
 module.exports = {
   buildImportItems,
   insertImport,
-  getNewLine
-};
+  getNewLine,
+}
