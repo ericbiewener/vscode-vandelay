5946827b18660cf8f792b75b4b06213d4baa9492
Eric Biewener <ebiewener@gmail.com>
2019-09-19 20:51:15 -0700

    Add config option for turning autocomplete suggestions on or off

--- src/initialization/finalizeExtensionActivation.ts
+++ src/initialization/finalizeExtensionActivation.ts
@@ -1,8 +1,13 @@
 import { commands, ExtensionContext, workspace } from 'vscode'
 import { cacheProject, watchForChanges } from '../cacher'
+import { DisposableKey, DisposableManager } from '../DisposableManager'
+import { registerCompletionItemProvider } from '../createCompletionItemProvider'
 import { importUndefinedVariables, selectImport, selectImportForActiveWord } from '../importer'
 import { initializePlugins } from '../main'
+import { PLUGINS } from '../plugins'
 import { removeUnusedImports } from '../removeUnusedImports'
+import { Plugin } from '../types'
+import { getConfiguration } from '../utils'
 import { catchError } from './catchError'
 
 let hasFinalized = false
@@ -42,6 +47,19 @@ export function finalizeExtensionActivation(context: ExtensionContext) {
       ) {
         initializePlugins(context)
       }
+
+      if (e.affectsConfiguration('vandelay.provideCompletions')) {
+        if (!getConfiguration().provideCompletions) {
+          DisposableManager.dispose(DisposableKey.PROVIDE_COMPLETIONS)
+        } else {
+          for (const k in PLUGINS) {
+            const plugin = PLUGINS[k] as unknown as Plugin
+            registerCompletionItemProvider(context, plugin)
+          }
+        }
+      }
+
+      
     }),
 
     watchForChanges()
